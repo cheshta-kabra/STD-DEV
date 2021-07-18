d23bf6ad78fe6b9d83c7ac782c64d5826fd73711
cheshta-kabra <64300181+cheshta-kabra@users.noreply.github.com>
2021-07-18 16:28:33 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,23 @@
+import csv
+import math
+with open('data2.csv',newline='') as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+totalMarks=0
+totalEnteries=len(file_data)
+print(totalEnteries)
+for marks in file_data:
+    totalMarks+=float(marks[1])
+mean=totalMarks/totalEnteries
+print('Mean is',mean)
+square_list=[]
+for number in file_data:
+    a=int(number[0])-mean
+    a=a**2
+    square_list.append(a)
+sum=0
+for i in square_list:
+    sum+=i
+result=sum/totalEnteries
+stdDeviation=math.sqrt(result)
+print (stdDeviation)
